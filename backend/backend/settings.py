386037d0dda993347ab04dd8b55fe6b132773cba
Junhong-Kim <jhk1005jhk@gmail.com>
2018-07-20 23:43:39 +0900

    Add: set proxy table

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
+
+STATICFILES_DIRS = (
+    os.path.join('static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
